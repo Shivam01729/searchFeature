9b1b18ce68604769ab5d30a052b3720f833fced8
Shashank01729 <117083031+Shashank01729@users.noreply.github.com>
2024-04-20 14:27:33 +0530

    port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const songs = require("./mongo");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
